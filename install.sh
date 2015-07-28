6a05102f52db893d1abd6e5128e3bf4abeb25764
Khatri <khatria@pun-khatri-m.ds.ad.adp.com>
2015-07-28 20:30:12 +0530

    added shell magic

--- install.sh
+++ install.sh
@@ -7,4 +7,13 @@ git submodule init
 git submodule update
 
 
+COUNT=$(grep -c "^source ~/.vim/.profile$" ~/.bash_profile)
+if [[ $COUNT = "0" ]]; then
+    #statements
+    echo "adding source to .bash_profile"
+    echo "source ~/.vim/.profile" >> ~/.bash_profile
+fi
+
+source ~/.bash_profile
+
 echo "You need to go and explicity build command-t. Follow installation instructions"
